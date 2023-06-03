edc4ddc246e2828a44c829ed502e734a150465e0
hackerswater <123229569+hackerswater@users.noreply.github.com>
2023-06-03 14:23:42 +0300

    Create express.js
    hello guys and this is how to hack crossy road..

---
+++ express.js
@@ -0,0 +1,30 @@
+# ftftftftftftftft
+yooguysthisismylolfcfycvughv9fcftcfccyguvu1ggcycycyfcghuvppemanmememememeoqnjhewuwneheejenecfywcycywcycfwctfcwcwsywh
+   throw open
+    const open
+     React open
+const game = crossy_road; //game = crossy_roasd
+ console.log(game)
+     function crossy_road{
+   class crossy_road{
+  crossy_road = poop_road //crossy_road = poop_road
+    
+  
+  
+  
+}
+  
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
