aca1c1fc232cac914437eee58bd7508f1715ebb1
hackerswater <123229569+hackerswater@users.noreply.github.com>
2023-06-06 16:19:49 +0300

    Create import.const.var.js
    helllo guys and this is js images .. and its a prank for you.. black man

---
+++ import.const.var.js
@@ -0,0 +1,6 @@
+# ftftftftftftftft
+yooguysthisismylolfcfycvughv9fcftcfccyguvu1ggcycycyfcghuvppemanmememememeoqnjhewuwneheejenecfywcycywcycfwctfcwcwsyw
+import { Images } from "https://th.bing.com/th/id/OIP.nVF8Ju3S7vo9fcwvmV-9VwHaFj?w=268&h=201&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+const Images = https//th.bing.com/th/id/OIP.nVF8Ju3S7vo9fcwvmV-9VwHaFj?w=268&h=201&c=7&r=0&o=5&dpr=1.3&pid=1.7
+throw change_to_website_to_app
+const change_website_to_app = //change_website_to_app = React app
