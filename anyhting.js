c57e484810628daef4ed88002b6201fe8e0fe57c
hackerswater <123229569+hackerswater@users.noreply.github.com>
2023-06-09 15:00:16 +0300

    Create anyhting.js
    this is the code for you dad how to hack roblox but only code how to put what is you in roblox and how to open it bye and this might be wrong i don t know..

---
+++ anyhting.js
@@ -0,0 +1,6 @@
+import { Carousel } from "Roblox"
+throw open //i don t know do code to open roblox
+const Carousel = Roblox
+import { CarouselCharacters } from "Roblox";
+const CarouselCharacters = qqqqq_qhi //do anyhting do your characters 
+
