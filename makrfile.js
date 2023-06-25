9e5ecd23f825202b2ac9dae759778dbe1aa67d3f
hackerswater <123229569+hackerswater@users.noreply.github.com>
2023-06-25 10:21:43 +0300

    Create makrfile.js
    this makes a window what is very cool and not dangreous and in a document

---
+++ makrfile.js
@@ -0,0 +1,41 @@
+# ftftftftftftftft
+yooguysthisismylolfcfycvughv9fcftcfccyguvu1ggcycycyfcghuvppemanmememememeoqnjhewuwneheejenecfywcycywcycfwctfcwcwsywh
+window.AbortController, window.AbortSignal, window.AbstractRange, window.ActiveXObject, window.AnalyserNode, window.Animation, window.AnimationEffect, window.AnimationEvent
+const Abo = window
+const Abs = window
+const Act = window
+const Animation = window
+const AnimationEffect = window
+const AnimationEvent = window
+window = AbortController, AbortSignal, AbstractRange, ActiveXObject, AnalyserNode, Animation, AnimationEffect, AnimationEvent
+throw  Window={Document}
+return
+toString
+window.AbortController, window.AbortSignal, window.AbstractRange, window.ActiveXObject, window.AnalyserNode, window.Animation, window.AnimationEffe
+Abo = window
+Abs = window
+Act = window
+Animation = window
+AnimationEffect = window
+AnimationEvent = window
+window = AbortController, AbortSignal, AbstractRange, ActiveXObject, AnalyserNode, Animation, AnimationEffect, AnimationEvent
+throw  window={PictureInPictureWindow}
+String
+//string 
+//string what makes a cool window
+//string what makes a cool window that has AbortController AbortSignal AbstractRange ActiveXObject AnalyserNode Animation AnimationEffect AnimationEvent
+//string what helps the code
+//the whole code is a string
+//its not html
+//and its a very smart window
+
+
+
+
+
+
+
+
+
+
+
